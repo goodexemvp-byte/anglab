4c1242e0bdb4d1a5bbfc109bfe46500375606958
goodexemvp-byte <goodexemvp@gmail.com>
2026-09-11 15:43:42 +0300

    Update message content for ticket notification

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ client.on('interactionCreate', async interaction => {
                 );
 
                 await ticketChannel.send({
-                    content: `منور يا <@${member.id}>، الإدارة هتكون معاك قريب.`,
+                    content: `منور يا <@${member.id}>، الموثوقين هيكونو معاك قريب.`,
                     embeds: [embed],
                     components: [ticketRow]
                 });
